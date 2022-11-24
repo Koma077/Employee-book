415324cc84be870d39140313079d0dd18cdd06c3
Koma077 <Sladkoeshka33@yandex.ru>
2022-11-24 23:06:57 +0700

    zxc

--- src/main/java/com/skypro/employee/controller/EmployeeController.java
+++ src/main/java/com/skypro/employee/controller/EmployeeController.java
@@ -1,2 +1,48 @@
-package com.skypro.employee.controller;public class EmployeeController {
+package com.skypro.employee.controller;
+
+import com.skypro.employee.model.Employee;
+import com.skypro.employee.record.EmployeeReqest;
+import com.skypro.employee.service.EmployeeService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.OptionalDouble;
+import java.util.OptionalInt;
+
+@RestController
+public class EmployeeController {
+    private  final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService){
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/employees")
+    public Collection<Employee> getAllEmployees(){
+        return this.employeeService.getAllEmployees();
+    }
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody EmployeeReqest employeeReqest){
+        return this.employeeService.addEmployee(employeeReqest);
+    }
+    @GetMapping("/employees/salary/sum")
+    public int getSalarySum(){
+        return this.employeeService.getSalarySum();
+    }
+    @GetMapping("/employees/salary/min")
+    public OptionalInt findMinSalary(){
+        return this.employeeService.findMinSalary();
+    }
+    @GetMapping("/employees/salary/max")
+    public OptionalInt findMaxSalary(){
+        return this.employeeService.findMaxSalary();
+    }
+    @GetMapping("/employees/high-salary")
+    public OptionalDouble findAverageSalary(){
+        return this.employeeService.findAverageSalary();
+    }
+    //
 }
--- src/main/java/com/skypro/employee/service/EmployeeService.java
+++ src/main/java/com/skypro/employee/service/EmployeeService.java
@@ -1,2 +1,44 @@
-package com.skypro.employee.service;public class EmployeeService {
+package com.skypro.employee.service;
+
+import com.skypro.employee.model.Employee;
+import com.skypro.employee.record.EmployeeReqest;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.OptionalInt;
+
+@Service
+public class EmployeeService {
+    private final Map<Integer, Employee> employees = new HashMap<>();
+
+    public Collection<Employee> getAllEmployees(){
+        return this.employees.values();
+    }
+
+    public Employee addEmployee(EmployeeReqest employeeReqest){
+        if(employeeReqest.getFirstName() == null || employeeReqest.getLastName() == null){
+            throw new IllegalArgumentException("Error");
+        }
+        Employee employee = new Employee(employeeReqest.getLastName(),
+                employeeReqest.getFirstName(),
+                employeeReqest.getDepartment(),
+                employeeReqest.getSalary());
+        this.employees.put(employee.getId(), employee);
+        return employee;
+    }
+    public int getSalarySum(){
+        return employees.values().stream().mapToInt(e->e.getSalary()).sum();
+    }
+    public OptionalInt findMinSalary() {
+        return employees.values().stream().mapToInt(e->e.getSalary()).min();
+    }
+    public OptionalInt findMaxSalary(){
+        return employees.values().stream().mapToInt(e->e.getSalary()).max();
+    }
+    public java.util.OptionalDouble findAverageSalary(){
+        return employees.values().stream().mapToInt(e->e.getSalary()).average();
+    }
 }
+
